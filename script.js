bfc7b2e2dde6feff29e37f7fc61daececadba006
vishnu santhosh <vishnusanthosh13579@gmail.com>
2026-09-08 00:58:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function revealOnInteraction() {
 
 midnightScreen.addEventListener("dblclick", revealOnInteraction);
 midnightScreen.addEventListener("wheel", revealOnInteraction, { passive: true });
+midnightScreen.addEventListener("touchmove", revealOnInteraction, { passive: true });
 
 // The midnight screen appears first.
 // If the visitor opens the site at any time other than exactly 00:00,
